3ba0df56e8f0d6503b410e07955b7ae9bc9d5051
dainoras99 <ziukasdainoras@gmail.com>
2021-05-15 19:39:29 +0300

    Add project files.

---
+++ Daugiagijio antras praktinis/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace Daugiagijio_antras_praktinis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.encrypt = new System.Windows.Forms.Button();
+            this.decrypt = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.pauseButton = new System.Windows.Forms.Button();
+            this.continueButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // encrypt
+            // 
+            this.encrypt.Location = new System.Drawing.Point(48, 40);
+            this.encrypt.Name = "encrypt";
+            this.encrypt.Size = new System.Drawing.Size(206, 37);
+            this.encrypt.TabIndex = 0;
+            this.encrypt.Text = "Šifruoti pasirinkto katalogo failus";
+            this.encrypt.UseVisualStyleBackColor = true;
+            this.encrypt.Click += new System.EventHandler(this.encrypt_Click);
+            // 
+            // decrypt
+            // 
+            this.decrypt.Location = new System.Drawing.Point(48, 107);
+            this.decrypt.Name = "decrypt";
+            this.decrypt.Size = new System.Drawing.Size(206, 37);
+            this.decrypt.TabIndex = 1;
+            this.decrypt.Text = "Dešfruoti pasirinkto katalogo failus";
+            this.decrypt.UseVisualStyleBackColor = true;
+            this.decrypt.Click += new System.EventHandler(this.decrypt_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 180);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(286, 23);
+            this.progressBar.TabIndex = 2;
+            // 
+            // pauseButton
+            // 
+            this.pauseButton.Location = new System.Drawing.Point(12, 231);
+            this.pauseButton.Name = "pauseButton";
+            this.pauseButton.Size = new System.Drawing.Size(75, 23);
+            this.pauseButton.TabIndex = 3;
+            this.pauseButton.Text = "PAUZĖ";
+            this.pauseButton.UseVisualStyleBackColor = true;
+            this.pauseButton.Click += new System.EventHandler(this.pauseButton_Click);
+            // 
+            // continueButton
+            // 
+            this.continueButton.Location = new System.Drawing.Point(117, 231);
+            this.continueButton.Name = "continueButton";
+            this.continueButton.Size = new System.Drawing.Size(75, 23);
+            this.continueButton.TabIndex = 4;
+            this.continueButton.Text = "TĘSTI";
+            this.continueButton.UseVisualStyleBackColor = true;
+            this.continueButton.Click += new System.EventHandler(this.continueButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(223, 231);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.TabIndex = 5;
+            this.stopButton.Text = "ATŠAUKTI";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(310, 276);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.continueButton);
+            this.Controls.Add(this.pauseButton);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.decrypt);
+            this.Controls.Add(this.encrypt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button encrypt;
+        private System.Windows.Forms.Button decrypt;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button pauseButton;
+        private System.Windows.Forms.Button continueButton;
+        private System.Windows.Forms.Button stopButton;
+    }
+}
+
---
+++ Daugiagijio antras praktinis/Form1.cs
@@ -0,0 +1,442 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+using System.IO.Compression;
+using System.Threading;
+
+
+namespace Daugiagijio_antras_praktinis
+{
+    public partial class Form1 : Form
+    {
+        ManualResetEvent manualResetEvent = new ManualResetEvent(true);
+        CancellationTokenSource cancellationTokenSource;
+        //private string[] filePaths;
+      
+        List<string> filePaths;
+        List<string> catalogPaths;
+        private string password = "dainorasdainorasdainorasdainoras";
+        private string IVkey = "1234567890ABCDEF";
+        List<string> md5HashValuesCalculatedBefore;
+        List<string> md5HashValuesCalculatedAfter;
+        private string mainFolderName;
+        private string mainFolderPath;
+        private string mainMD5ValuesPath;
+        private int progressBarCount = 0;
+        CancellationToken cancellationToken;
+        private List<string> revokeZip;
+        List<string> revoke;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void validationForEncryption()
+        {
+            string path = @"D:\KOLEGIJOS MEDZIAGA\DAUGIAGIJIS PROGRAMAVIMAS\Daugiagijio antras praktinis\md5 values";
+            string newFile = path + "\\MD5 hash values for folder_" + mainFolderName;
+            if (File.Exists(newFile))
+                throw new Exception("Šie failai jau užšifruoti");
+        }
+
+        private void validationForDecryption()
+        {
+                string path = @"D:\KOLEGIJOS MEDZIAGA\DAUGIAGIJIS PROGRAMAVIMAS\Daugiagijio antras praktinis\md5 values";
+                string newFile = path + "\\MD5 hash values for folder_" + mainFolderName;
+                if (!File.Exists(newFile))
+                    throw new Exception("Šie failai nebuvo užšifruoti");
+        }
+
+        private void encrypt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                mainFolderName = null;
+                filePaths = new List<string>();
+                catalogPaths = new List<string>();
+                progressBarCount = 0;
+                cancellationTokenSource = new CancellationTokenSource();
+                getFilesFromFolderEncrypt();
+                if (mainFolderName != null)
+                {
+                    validationForEncryption();
+                    md5HashValuesCalculatedBefore = new List<string>();
+                    md5HashValuesCalculatedAfter = new List<string>();
+                    ZipDirectories();
+                    cancellationToken = cancellationTokenSource.Token;
+                    Thread encryptionThread = new Thread(encryptMethods);
+                    Thread progressBarThread = new Thread(() => progressBarValue(encryptionThread));
+                    progressBarThread.Start();
+                    encryptionThread.Start();
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+        private void decrypt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                mainFolderName = null ;
+                md5HashValuesCalculatedBefore = new List<string>();
+                md5HashValuesCalculatedAfter = new List<string>();
+                filePaths = new List<string>();
+                progressBarCount = 0;
+                getFilesFromFolderDecrypt();
+                if (mainFolderName != null)
+                {
+                    validationForDecryption();
+                    md5HashValuesCalculatedBefore = new List<string>();
+                    md5HashValuesCalculatedAfter = new List<string>();
+                    cancellationTokenSource = new CancellationTokenSource();
+                    cancellationToken = cancellationTokenSource.Token;
+                    Thread decryptionThread = new Thread(decryptMethods);
+                    Thread progressBarThread = new Thread(() => progressBarValue(decryptionThread));
+                    progressBarThread.Start();
+                    decryptionThread.Start();
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private void decryptMethods()
+        {
+            SearchTextFile();
+            ReadMD5ValuesFromFile();
+            DecryptEveryFile();
+        }
+        
+        private void encryptMethods()
+        {
+            mainMD5ValuesPath = CreateTextFileForSavingMD5Values();
+            EncryptEveryFile();
+        }
+
+        private void progressBarValue(Thread thread)
+        {
+            progressBarSettings();
+            while (true)
+            {
+                this.Invoke((Action)delegate
+                {
+                    progressBar.Value = progressBarCount;
+                });
+               
+                if (!thread.IsAlive)
+                {
+                    progressBarCount = 0;
+                    break;
+                }
+            }
+        }
+
+        private void progressBarSettings()
+        {
+            this.Invoke((Action)delegate
+            {
+                progressBar.Minimum = 0;
+                progressBar.Maximum = filePaths.Count;
+                progressBar.Value = 0;
+            });
+        }
+
+        private void getFilesFromFolderEncrypt()
+        {
+
+            FolderBrowserDialog browserDialog = new FolderBrowserDialog();
+            if (browserDialog.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string catalog in Directory.GetDirectories(browserDialog.SelectedPath, "*", SearchOption.TopDirectoryOnly))
+                    catalogPaths.Add(catalog);
+                foreach (string file in Directory.GetFiles(browserDialog.SelectedPath))
+                    filePaths.Add(file);
+                mainFolderName = Path.GetFileName(browserDialog.SelectedPath);
+                mainFolderPath = Path.GetFullPath(browserDialog.SelectedPath);
+            }
+            else
+                return;
+        }
+
+        private void getFilesFromFolderDecrypt()
+        {
+
+            FolderBrowserDialog browserDialog = new FolderBrowserDialog();
+            if (browserDialog.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string file in Directory.GetFiles(browserDialog.SelectedPath))
+                    filePaths.Add(file);
+                mainFolderName = Path.GetFileName(browserDialog.SelectedPath);
+                mainFolderPath = Path.GetFullPath(browserDialog.SelectedPath);
+            }
+        }
+
+        private void ZipDirectories()
+        {
+            revokeZip = new List<string>();
+            foreach (string catalog in catalogPaths)
+            {
+                ZipFile.CreateFromDirectory(catalog, catalog + ".rar");
+                Directory.Delete(catalog, true);
+                filePaths.Add(catalog + ".rar");
+                revokeZip.Add(catalog + ".rar");
+            }
+        }
+
+       
+        private void EncryptEveryFile()
+        {
+            filePaths.Sort();
+            revoke = new List<string>();
+            foreach (string file in filePaths)
+            {
+                revoke.Add(file);
+                manualResetEvent.WaitOne();
+                Encrypt(file);
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    foreach (string revokingFile in revoke)
+                    {
+                        Decrypt(revokingFile + ".aes");
+                    }
+                    foreach (string folder in revokeZip)
+                    {
+                        ZipFile.ExtractToDirectory(folder, folder.Remove(folder.Length - 4));
+                        File.Delete(folder);
+                    }
+                    File.Delete(mainMD5ValuesPath);
+                    progressBarCount = 0;
+                    this.Invoke((Action)delegate
+                    {
+                        MessageBox.Show("Veiksmai atšaukti, grąžinta pradinė stadija");
+                    });
+                    return;
+                }
+                progressBarCount++;
+                Thread.Sleep(250);
+            }
+            this.Invoke((Action)delegate
+            {
+                MessageBox.Show("Šifravimas baigtas");
+            });
+        }
+
+        private void DecryptEveryFile()
+        {
+            filePaths.Sort();
+            revoke = new List<string>();
+            revokeZip = new List<string>();
+            foreach (string file in filePaths)
+            {
+                CalculateMD5HashValuesSecondTime(file);
+            }
+
+            int i = -1;
+            foreach (string file in filePaths)
+            {
+                revoke.Add(file);
+                manualResetEvent.WaitOne();
+                i++;
+                if (md5HashValuesCalculatedBefore != null && md5HashValuesCalculatedAfter != null)
+                {
+                    if (md5HashValuesCalculatedBefore.ElementAt(i).Equals(md5HashValuesCalculatedAfter.ElementAt(i)))
+                    {
+                        Decrypt(file);
+                        if (file.Contains(".rar"))
+                        {
+                            revokeZip.Add(file);
+                            ZipFile.ExtractToDirectory(file.Remove(file.Length - 4), file.Remove(file.Length - 8));
+                            File.Delete(file.Remove(file.Length - 4));
+                        }
+                        progressBarCount++;
+                        Thread.Sleep(250);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nesutampa hashai");
+                    }
+                    if (cancellationToken.IsCancellationRequested)
+                    {
+                        foreach (string folder in revokeZip)
+                        {
+                            ZipFile.CreateFromDirectory(folder.Remove(folder.Length - 8), folder.Remove(folder.Length - 4));
+                            Directory.Delete(folder.Remove(folder.Length - 8), true);
+                        }
+                        foreach (string revokingFile in revoke)
+                        {
+                            Encrypt(revokingFile.Remove(revokingFile.Length - 4));
+                        }
+                        progressBarCount = 0;
+                        md5HashValuesCalculatedAfter = null;
+                        md5HashValuesCalculatedBefore = null;
+                        this.Invoke((Action)delegate
+                        {
+                            MessageBox.Show("Veiksmai atšaukti, grąžinta pradinė stadija");
+                        });
+                        return;
+                    }
+                }
+            }
+            DeleteMD5HashFile();
+            this.Invoke((Action)delegate
+            {
+                MessageBox.Show("Dešifravimas baigtas");
+            });
+        }
+
+        private void DeleteMD5HashFile()
+        {
+            File.Delete(mainMD5ValuesPath);
+        }
+        private string CreateTextFileForSavingMD5Values ()
+        {
+            string path = @"D:\KOLEGIJOS MEDZIAGA\DAUGIAGIJIS PROGRAMAVIMAS\Daugiagijio antras praktinis\md5 values";
+            string newFile = path + "\\MD5 hash values for folder_" + mainFolderName;
+            var create = System.IO.File.Create(newFile);
+            create.Dispose();
+            return newFile;
+        }
+
+        private void CalculateAndSaveMD5(string file)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(file))
+                {
+                    var hash = md5.ComputeHash(stream);
+                    using (StreamWriter writer = File.AppendText(mainMD5ValuesPath))
+                    {
+                        writer.Write(BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant());
+                        writer.Write("\n");
+                    }
+                }
+            }
+        }
+
+        private void SearchTextFile()
+        {
+            string path = @"D:\KOLEGIJOS MEDZIAGA\DAUGIAGIJIS PROGRAMAVIMAS\Daugiagijio antras praktinis\md5 values";
+            foreach (string file in Directory.GetFiles(path))
+            {
+                if (file == path + "\\MD5 hash values for folder_" + mainFolderName)
+                {
+                    mainMD5ValuesPath = path + "\\MD5 hash values for folder_" + mainFolderName;
+                }
+            }
+        }
+
+        private void ReadMD5ValuesFromFile()
+        {
+            md5HashValuesCalculatedBefore = File.ReadAllLines(mainMD5ValuesPath).ToList();
+        }
+
+        private void CalculateMD5HashValuesSecondTime(string file)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(file))
+                {
+                    var hash = md5.ComputeHash(stream);
+                    md5HashValuesCalculatedAfter.Add(BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant());
+                }
+            }
+        }
+
+
+        private void Encrypt(string file)
+        {
+            try
+            {
+                byte[] plainContent = File.ReadAllBytes(file);
+                using (var AES = new RijndaelManaged())
+                {
+                    AES.IV = Encoding.UTF8.GetBytes(IVkey);
+                    AES.Key = Encoding.UTF8.GetBytes(password);
+                    AES.Mode = CipherMode.CBC;
+                    AES.Padding = PaddingMode.PKCS7;
+
+                    using (var memStream = new MemoryStream())
+                    {
+                        CryptoStream cryptoStream = new CryptoStream(memStream, AES.CreateEncryptor(), CryptoStreamMode.Write);
+
+                        cryptoStream.Write(plainContent, 0, plainContent.Length);
+                        cryptoStream.FlushFinalBlock();
+                        File.WriteAllBytes(file, memStream.ToArray());
+
+                        string addedExtension = file + ".aes";
+                        File.Move(file, addedExtension);
+
+                        CalculateAndSaveMD5(file + ".aes");
+
+                        Console.WriteLine("Encrypted succesfully" + file);
+                    }
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private void Decrypt(string file)
+        {
+            try
+            {
+                byte[] plainContent = File.ReadAllBytes(file);
+                using (var AES = new RijndaelManaged())
+                {
+                    AES.IV = Encoding.UTF8.GetBytes(IVkey);
+                    AES.Key = Encoding.UTF8.GetBytes(password);
+                    AES.Mode = CipherMode.CBC;
+                    AES.Padding = PaddingMode.PKCS7;
+
+                    using (var memStream = new MemoryStream())
+                    {
+                        CryptoStream cryptoStream = new CryptoStream(memStream, AES.CreateDecryptor(), CryptoStreamMode.Write);
+
+                        cryptoStream.Write(plainContent, 0, plainContent.Length);
+                        cryptoStream.FlushFinalBlock();
+                        File.WriteAllBytes(file, memStream.ToArray());
+
+                        string removeExtension = Path.ChangeExtension(file, null);
+                        File.Move(file, removeExtension);
+
+                        Console.WriteLine("Decrypted succesfully" + file);
+                    }
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private void pauseButton_Click(object sender, EventArgs e)
+        {
+            manualResetEvent.Reset();
+        }
+
+        private void continueButton_Click(object sender, EventArgs e)
+        {
+            manualResetEvent.Set();
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            cancellationTokenSource.Cancel();
+        }
+    }
+}
